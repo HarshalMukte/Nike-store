3ea94c76a87eb3d5ef1004b828ecf44f992bc7f0
Harshal <harshalmukte@gmail.com>
2024-04-23 20:18:36 +0530

    initial commit

---
+++ client/src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-black text-white w-full h-screen items-center justify-center flex">
+      <h1 className="text-center">This works!!!!</h1>
+    </div>
+  );
+}
